f8c6fd7abd3fd81b5ca60de65df6eddf559aa4e0
Soniasonia6 <43379274+Soniasonia6@users.noreply.github.com>
2020-06-03 17:41:25 -0400

    second commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(string category)
+        {
+           
+            // store the selected category name in the ViewBagg container
+            //ViewBag is automatically passed back to the view that loads
+            ViewBag.category = category;
+            return View();
+    }
+}
+}
